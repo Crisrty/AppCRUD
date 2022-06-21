fb0ceb46e943bb75e9359f957294475b354f2282
Cristina Reyes <cristinabm.rs@gmail.com>
2022-06-20 19:35:47 -0500

    primer javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+const contactoForm = document.getElementById('contacto-form'); 
+const inputNombre = document.getElementById('inputNombre'); 
+const inputApellido = document.getElementById('inputApellido'); 
+const inputTarea = document.getElementById('inputTarea'); 
+const bodyTabla = document.getElementById('body-tabla'); 
